158105628d96f0434cdabf60b57b273b16d1a4f2
Mohamed Abdul <mohamedyousufmo@gmail.com>
2023-11-12 23:27:56 -0800

    refactored

--- Project-2/Utills.py
+++ Project-2/Utills.py
@@ -5,9 +5,16 @@ def compute_shortest_path(adjacencyData : MatrixWrapper, vertex, targetNode) -> 
     return dfs(adjacencyData, vertex, targetNode, visitedNodes)
 
 
+import sys
+
 def dfs(matrixWrapper, vertex, targetNode, visitedNodes):
+    # Initialize visitedNodes if not done already
+    if visitedNodes is None:
+        visitedNodes = {}
+
+    # Base case: Check if the current vertex is the target node
     if vertex == targetNode:
-        return sys.maxsize - 50
+        return 0
 
     # Mark the current node as visited
     visitedNodes[vertex] = True
@@ -15,10 +22,12 @@ def dfs(matrixWrapper, vertex, targetNode, visitedNodes):
     # Get all edges of the vertex
     edges = matrixWrapper.getEdges(vertex)
 
-    minPath = sys.maxsize - 50
+    minPath = sys.maxsize
 
     for edge in edges:
+        # Check if the edge is not visited
         if edge not in visitedNodes or not visitedNodes[edge]:
+            # Recursively find the path length
             minPath = min(1 + dfs(matrixWrapper, edge, targetNode, visitedNodes), minPath)
 
-    return minPath
+    return minPath if minPath < sys.maxsize else -1  # Return -1 for unreachable targetNode
